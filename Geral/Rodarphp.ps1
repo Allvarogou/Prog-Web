a90e918ce5da98b44a83c9e58a5da1886b88d633
Allvarogou <alvaro.gr@aluno.ifsc.edu.br>
2025-11-11 11:11:21 -0300

    Teste

---
+++ Geral/Rodarphp.ps1
@@ -0,0 +1,18 @@
+cd /
+cd xampp
+./
+code ./
+cd
+cd gtdocs
+cd gtdocs
+cd htdocs
+cd ..
+git clone https://github.com/Allvarogou/Prog-Web
+cd htdocs
+code ./
+git status
+git init
+git remote add origin https://github.com/Allvarogou/Prog-Web
+git fetch origin
+git checkout main
+
